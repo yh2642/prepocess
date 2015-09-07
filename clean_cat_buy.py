3f798c88925494b84c5aab07d4226f4139d8e3b6
yh2642 <yh2642@columbia.edu>
2015-09-07 13:15:06 +0800

    9.7

--- clean_cat_buy.py
+++ clean_cat_buy.py
@@ -7,13 +7,15 @@ import urllib
 import sys
 import MySQLdb
 import time
+import redis
+
 
 def extract_from_db():
     conn=MySQLdb.connect(host='localhost',user='root',passwd='root',db='test1',port=3306, charset='utf8')
     cur=conn.cursor()
     t1 = time.clock()
     cur.execute("""SET NAMES utf8""")
-    data = cur.execute("""select buyofferId, product, amount,category from buy_offer where memberid = 'czlixingg';""")
+    data = cur.execute("""select DISTINCT product from buy_offer where memberid = 'czlixingg';""")
     t2 = time.clock()
     print u'成功提取信息'
     print 'Time used=%fs'%(t2-t1)
@@ -25,6 +27,12 @@ def extract_from_db():
 
 
 if __name__ == '__main__':
+    r = redis.Redis()
     for ele in extract_from_db():
-        print urllib.quote(ele[1].encode('gbk'))
+        quote_string = urllib.quote(ele[0].encode('gbk'))
+        print quote_string
+        print urllib.unquote(quote_string).decode('gbk')
+        print 'http://s.1688.com/selloffer/offer_search.htm?keywords=' + quote_string
+        r.lpush('cat_query:start_urls', 'http://s.1688.com/selloffer/offer_search.htm?keywords=' + quote_string)
+        print '+1'
 
--- sep_offer_info.py
+++ sep_offer_info.py
@@ -14,7 +14,9 @@ class dister(threading.Thread): #The timer class is derived from the class threa
 	def run(self):
 		print u"开始进行线程:%s" % self.key
 		for ele in self.zones:
-			self.out_table(ele)
+			self.create_sub_tables(self.key)
+			self.dist_table(self.key, ele)
+			#self.out_table(ele)
 		print u"线程%s 结束！！！" % self.key
 
 	def out_table(self, zone):
@@ -24,13 +26,40 @@ class dister(threading.Thread): #The timer class is derived from the class threa
 		cur=conn.cursor()
 		path_name = 'c:/data_set/' + zone[0]+ zone[1]+'.txt'
 		cur.execute("""SET NAMES utf8""")
-		cur.execute("""select memberid, category, sale,qualitylevel from temp_info2 where memberid >= %s and memberid < %s
+		cur.execute("""select memberid, category, sale,qualitylevel from temp_info where memberid >= %s and memberid < %s
 		  				into outfile %s fields terminated by ',' lines terminated by '\r\n';""", (zone[0], zone[1], path_name))
 		t2 = time.clock()
 		print u'成功导入信息%s' % path_name
 		print 'Time used=%fs'%(t2-t1)
 		conn.commit()
 		conn.close()
+
+	def dist_table(self, key, zone):
+		print u'开始导出%s, %s' % (zone[0], zone[1])
+		t1 = time.clock()
+		conn=MySQLdb.connect(host='localhost',user='root',passwd='root',db='test2',port=3306, charset='utf8')
+		cur=conn.cursor()
+		cur.execute("""SET NAMES utf8""")
+		cur.execute("""Insert into %s select * from offer_info where memberid >= '%s' and memberid < '%s';""" % ('offer_info' + key, zone[0], zone[1]))
+		t2 = time.clock()
+		print u'成功导入数据库offer_info%s' % key
+		print 'Time used=%fs'%(t2-t1)
+		conn.commit()
+		conn.close()
+
+
+	def create_sub_tables(self, key):
+		table_name = 'offer_info' + key
+		#sql = '''DROP TABLE %s;''' % ele
+		sql = '''CREATE TABLE %s(offerId CHAR(12) NOT NULL PRIMARY KEY,type CHAR(4) NOT NULL,memberid VARCHAR(22) NOT NULL,subject VARCHAR(100),category CHAR(9) NOT NULL,company VARCHAR(50) NOT NULL,sale mediumint,status VARCHAR(20), qualitylevel TINYINT)ENGINE=InnoDB DEFAULT CHARSET=utf8;''' % table_name
+		conn=MySQLdb.connect(host='localhost',user='root',passwd='root',db='test2',port=3306, charset='utf8')
+		cur=conn.cursor()
+		cur.execute(sql)
+		conn.commit()
+		print u'成功创建数据表: %s' % table_name
+		conn.close()
+
+
 
 
 def test(dict, num_of_threads = 8):
@@ -79,6 +108,8 @@ def det_table(memberid):
 		else:
 			return 'offer_info' + 'b2b' + 'other'
 
+
+'''
 def out_part_table(dict):
 	for key in dict:
 		for zone in dict[key]:
@@ -93,36 +124,18 @@ def out_part_table(dict):
 			print 'Time used=%fs'%(t2-t1)
 			conn.commit()
 			conn.close()
-
-def dist_part_table(dict):
-	for key in dict:
-		for zone in dict[key]:
-			t1 = time.clock()
-			conn=MySQLdb.connect(host='localhost',user='root',passwd='root',db='test2',port=3306, charset='utf8')
-			cur=conn.cursor()
-			cur.execute("""SET NAMES utf8""")
-			cur.execute("""Insert into %s select * from temp_info2 where memberid >= '%s' and memberid < '%s';""" % ('offer_info' + key, zone[0], zone[1]))
-			t2 = time.clock()
-			print u'成功导入数据库offer_info%s' % key
-			print 'Time used=%fs'%(t2-t1)
-			conn.commit()
-			conn.close()
+'''
 
 
 
+
+
+
+
+def gen_table_dict():
 	'''
-	for indx in range(1, len(char_ls)):
-		conn=MySQLdb.connect(host='localhost',user='root',passwd='root',db='test2',port=3306, charset='utf8')
-		cur=conn.cursor()
-		cur.execute("""SET NAMES utf8""")
-		cur.execute("""select memberid, category, qualitylevel from offer_info where memberid >= %s and memberid < %s""", (char_ls[indx-1], char_ls[indx]))
-
-	print char_ls
+	:return: 对memberid进行分块处理，返回table_dict中，key为表名，value是区间
 	'''
-
-
-
-if __name__ == '__main__':
 	# name list
 	b2b_12 = ['b2b'+ str(ele/2) for ele in range(100, 300, 2)]
 	# insert other zone
@@ -136,8 +149,8 @@ if __name__ == '__main__':
 	name_ls.extend(num_ls)
 	name_ls.extend(b2b_12)
 	name_ls.extend(char_ls)
-	print name_ls
-	# index list
+	print name_ls #生成key list
+	# index list   区间列表
 	char_indx = [chr(i) for i in range(99,123)]
 	b2b_ls_indx = ['b2b-' + str(ele) for ele in range(100, 300, 2)]
 	b2b_ls_indx.append('b2b-300')
@@ -151,20 +164,27 @@ if __name__ == '__main__':
 	indx_ls.append('zzzzz')
 	print indx_ls
 
+	# 生成区间字典
 	table_dict = {}
 	for indx in range(len(name_ls)):
 		if name_ls[indx] in table_dict:
 			table_dict[name_ls[indx]].append((indx_ls[indx], indx_ls[indx + 1]))
 		else:
 			table_dict[name_ls[indx]] = [(indx_ls[indx], indx_ls[indx + 1])]
+
+	return table_dict
+
+
+if __name__ == '__main__':
+	#print gen_table_dict()
 	#print table_dict
-	#dict = {'b2b78': [('b2b-156', 'b2b-158')]}
+	dict = {'b2b78': [('b2b-156', 'b2b-158')]}
 	#create_sub_tables(table_dict)
 	#dist_part_table(dict)
-	t1 = time.clock()
-	test(table_dict, num_of_threads=8)
+	#t1 = time.clock()
+	test(dict, num_of_threads=8)
 	#out_part_table(table_dict)
-	t2 = time.clock()
-	print 'Time total used=%fs'%(t2-t1)
+	#t2 = time.clock()
+	#print 'Time total used=%fs'%(t2-t1)
 
 
